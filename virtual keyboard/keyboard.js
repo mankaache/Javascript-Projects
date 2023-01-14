ac186f2edd65a6fd82efa707fcbdc270ad7d8373
mankaa <mankaache10@gmail.com>
2023-01-14 20:01:16 +0100

    initialising functions

--- virtual keyboard/keyboard.js
+++ virtual keyboard/keyboard.js
@@ -8,5 +8,29 @@ const keyboard = {
     eventHandlers:{
         oninput:null,
         onclose:null
+    },
+    properties:{
+        value:"",
+        capslock:false
+    },
+
+
+    init(){
+        //when we load page
+    },
+    _createKeys(){
+        //creating html for the keys
+    },
+    _triggerevents(handlerName){
+
+    },
+    _toggleCapslock(){
+
+    },
+    open(initialValue,oninput,onclose){
+
+    },
+    close(){
+
     }
 }
